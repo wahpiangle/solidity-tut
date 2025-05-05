5a14cb41cb1a6dab9812dac443ea29b99dbb3f63
wahpiangle <qmtan17@gmail.com>
2025-05-05 18:40:18 +0800

    init

---
+++ contracts/Calculator.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Calculator{
+    uint256 res = 0;
+
+    function add(uint256 _num1) public {
+        res += _num1;
+    }
+    
+    function substract(uint256 _num1) public {
+        res -= _num1;
+    }
+
+    function multiply(uint256 _num1) public {
+        res *= _num1;
+    }
+    
+    function divide(uint256 _num1) public {
+        res /= _num1;
+    }
+
+    //view = not modifying state
+    function getResult() public view returns (uint256){
+        return res;
+    }
+}
